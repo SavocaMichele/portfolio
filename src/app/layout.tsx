17c3d4954d8564010b22acb0e23919dc6b3d2805
Michele Savoca <Michele.savoca@me.com>
2026-04-08 14:29:20 +0200

    + Added back site.webmanifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,11 +54,6 @@ export default function RootLayout({children}: Readonly<{ children: React.ReactN
     return (
         <html lang="en" className={"no-js"} suppressHydrationWarning>
             <head>
-                <link rel="icon" type="image/png" href="/favicon-96x96.png?v=20260408" sizes="96x96" />
-                <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=20260408" />
-                <link rel="shortcut icon" href="/favicon.ico?v=20260408" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=20260408" />
-
                 <meta name="apple-mobile-web-app-title" content="Portoflio" />
                 <link rel="manifest" href="/site.webmanifest?v=20260408" />
 
